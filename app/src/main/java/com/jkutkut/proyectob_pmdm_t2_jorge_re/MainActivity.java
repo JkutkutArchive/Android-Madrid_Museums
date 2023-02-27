efef7907331935edfd93d6126336eb40e6a56802
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-27 08:09:16 +0100

    Starting activity fully created

--- app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/MainActivity.java
+++ app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/MainActivity.java
@@ -1,14 +1,26 @@
 package com.jkutkut.proyectob_pmdm_t2_jorge_re;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.widget.AppCompatButton;
 
+import com.jkutkut.proyectob_pmdm_t2_jorge_re.custom.CustomActivity;
+
+public class MainActivity extends CustomActivity {
+
+    private AppCompatButton btnStart;
+
+    @SuppressLint("MissingSuperCall")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState, R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(v -> {
+//            Intent i = new Intent(this, QueryActivity.class);
+//            startActivity(i);
+        });
     }
 }
