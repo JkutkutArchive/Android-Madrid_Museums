164ad637d52d6fb3820b9c49bc7ac5617be849f2
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-03-01 16:32:57 +0100

    Now obtaining the data we already have using the API :S

--- app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/MuseumDetailActivity.java
+++ app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/MuseumDetailActivity.java
@@ -1,25 +1,69 @@
 package com.jkutkut.proyectob_pmdm_t2_jorge_re;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.jkutkut.proyectob_pmdm_t2_jorge_re.api.MuseumAPI;
+import com.jkutkut.proyectob_pmdm_t2_jorge_re.api.RetrofitClient;
 import com.jkutkut.proyectob_pmdm_t2_jorge_re.api.result.Museum;
+import com.jkutkut.proyectob_pmdm_t2_jorge_re.api.result.MuseumResultAPI;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MuseumDetailActivity extends AppCompatActivity {
 
     public static final String ARG = "museum";
-
-    private Museum museum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_museum_detail);
 
-        museum = (Museum) getIntent().getSerializableExtra(ARG);
+        Museum museum = (Museum) getIntent().getSerializableExtra(ARG);
 
+        String[] id_split = museum.getAtId().split("/");
+        String id = id_split[id_split.length - 1];
+
+//        loadData(museum);
+
+        getData(
+            id,
+            new Callback<MuseumResultAPI>() {
+                @Override
+                public void onResponse(@NonNull Call<MuseumResultAPI> call, @NonNull Response<MuseumResultAPI> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(getBaseContext(), "Error: " + response.code(), Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    MuseumResultAPI result = response.body();
+                    assert result != null;
+                    loadData(result.getMuseums().get(0));
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<MuseumResultAPI> call, @NonNull Throwable t) {
+                    Toast.makeText(getBaseContext(), "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    throw new RuntimeException(t);
+                }
+            }
+        );
+    }
+
+    private void getData(String museum_id, Callback<MuseumResultAPI> callback) {
+        Retrofit r = RetrofitClient.getClient(MuseumAPI.URL);
+        MuseumAPI museumAPI = r.create(MuseumAPI.class);
+        Call<MuseumResultAPI> call = museumAPI.getMuseum(museum_id);
+        call.enqueue(callback);
+    }
+
+    private void loadData(Museum museum) {
         TextView txtvName = findViewById(R.id.txtvName);
         TextView txtvAddress = findViewById(R.id.txtvAddress);
         TextView txtvDescription = findViewById(R.id.txtvDescription);
@@ -27,16 +71,16 @@ public class MuseumDetailActivity extends AppCompatActivity {
 
         txtvName.setText(museum.getTitle());
         txtvAddress.setText(String.format(
-            getString(R.string.address_template),
-            museum.getAddress().getStreetAddress(),
-            museum.getAddress().getPostalCode(),
-            museum.getAddress().getLocality()
+                getString(R.string.address_template),
+                museum.getAddress().getStreetAddress(),
+                museum.getAddress().getPostalCode(),
+                museum.getAddress().getLocality()
         ));
         txtvDescription.setText(
-            museum.getOrganization().getOrganizationDesc()
+                museum.getOrganization().getOrganizationDesc()
         );
         txtvSchedule.setText(
-            museum.getOrganization().getSchedule()
+                museum.getOrganization().getSchedule()
         );
     }
 }
--- app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/api/MuseumAPI.java
+++ app/src/main/java/com/jkutkut/proyectob_pmdm_t2_jorge_re/api/MuseumAPI.java
@@ -4,6 +4,7 @@ import com.jkutkut.proyectob_pmdm_t2_jorge_re.api.result.MuseumResultAPI;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MuseumAPI {
@@ -12,5 +13,10 @@ public interface MuseumAPI {
     @GET("201132-0-museos.json")
     Call<MuseumResultAPI> getMuseums(
         @Query("distrito_nombre") String district
+    );
+
+    @GET("tipo/entidadesyorganismos/{museum}")
+    Call<MuseumResultAPI> getMuseum(
+        @Path("museum") String museum_id
     );
 }
